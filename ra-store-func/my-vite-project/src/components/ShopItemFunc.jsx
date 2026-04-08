93e36bc98265b879980b7651fc9ca10fc2689d3b
Роман Данилов <danroman@rambler.ru>
2026-04-08 05:29:56 +0300

    create folder

---
+++ ra-store-func/my-vite-project/src/components/ShopItemFunc.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+function ShopItemFunc({ item }) {
+    const { brand, title, description, descriptionFull, price, currency } = item
+
+    const formattedPrice = price.toFixed(2)
+
+    return (
+        <div className="main-content">
+            <div className="main-content_label">
+                <h2>{brand}</h2>
+                <h1>{title}</h1>
+                <h3>{description}</h3>
+                <div className="description">
+                    {descriptionFull}
+                </div>
+                <div className="highlight-window mobile">
+                    <div className="highlight-overlay"></div>
+                </div>
+                <div className="divider"></div>
+                <div className="purchase-info">
+                    <div className="price">
+                        {currency}{formattedPrice}
+                    </div>
+                    <button>Добавить в корзину</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ShopItemFunc
